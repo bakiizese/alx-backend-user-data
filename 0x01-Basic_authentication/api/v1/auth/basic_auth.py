ca1c66097834cfb7f781477da59e9d1424868419
root <bereketzeselassie@gmail.com>
2024-05-21 03:10:43 +0000

    ..

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+''' basic auth class '''
+from api.v1.auth.auth import Auth
+import base64
+
+
+class BasicAuth(Auth):
+    ''' class '''
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        ''' check if can be encode '''
+        if not authorization_header or not isinstance(authorization_header, str):
+            return None
+
+        ls = authorization_header.split(' ')
+
+        if ls[0] != 'Basic':
+            return None
+        auths = str(ls[1])
+        return auths
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        ''' decode to base64 '''
+        if not base64_authorization_header or not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            base = base64.b64decode(base64_authorization_header)
+            baseutf = base.decode('utf-8')
+        except Exception:
+            return None
+        return baseutf
+
