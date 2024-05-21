5a2b3db114eb1ac87b43d393ce16357d71f8ec4d
root <bereketzeselassie@gmail.com>
2024-05-21 12:51:20 +0000

    authenticated

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 ''' basic auth class '''
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -31,11 +33,12 @@ class BasicAuth(Auth):
 
         try:
             base = base64.b64decode(base64_authorization_header)
-            baseutf = base.decode('utf-8')
         except Exception:
+            print('her')
             return None
 
-        return baseutf
+        baseutf = base.decode('utf-8')
+        return base
 
     def extract_user_credentials(self,
                                  decoded_base64_authorization_header:
@@ -48,3 +51,24 @@ class BasicAuth(Auth):
             return (None, None)
         sp = decoded_base64_authorization_header.split(':')
         return (sp[0], sp[1])
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        ''' get user '''
+        if (
+                not user_email or not user_pwd or
+                not isinstance(user_email, str) or
+                not isinstance(user_pwd, str)
+                ):
+            return None
+        dicts = {'email': user_email}
+
+        if not User.search(dicts):
+            return None
+        based_on_email = User.search(dicts)
+        based_on_email = based_on_email[0]
+        if based_on_email.is_valid_password(user_pwd):
+            dicts['password'] = based_on_email.password
+            search_based_em_pwd = User.search(dicts)
+            return search_based_em_pwd[0]
+        return None
