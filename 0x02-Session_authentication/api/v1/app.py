5b57f962c52ca8b0c2bf0bfe86cf98eb962eb7e5
root <bereketzeselassie@gmail.com>
2024-05-23 11:52:06 +0000

    try

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -68,4 +68,4 @@ def before_request():
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=0)
+    app.run(host=host, port=port, debug=1)
