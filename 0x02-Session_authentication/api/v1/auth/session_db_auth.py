ed0ae5f2259bf9f95bade8cdb8d648ca1ed47288
root <bereketzeselassie@gmail.com>
2024-05-23 16:21:05 +0000

    save_to_file

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -2,6 +2,7 @@
 ''' session db '''
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
+from datetime import datetime, timedelta
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -10,19 +11,36 @@ class SessionDBAuth(SessionExpAuth):
         ''' create usersession instance '''
         session_id = super().create_session(user_id)
         if session_id:
-            user_session = UserSession(user_id, session_id)
+            kwarg = {'user_id': user_id, 'session_id': session_id}
+            user_session = UserSession(**kwarg)
+            user_session.save()
+            user_session.save_to_file()
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
         ''' returns user_id by session_id in db '''
-        usr_id = super().user_id_for_session_id(session_id)
-        if usr_id:
-            return usr_id
-        return None
+        if not session_id:
+            return None
+        UserSession.load_from_file()
+        g = UserSession.search({'session_id': session_id})
+        if not g:
+            return None
+        g = g[0]
+        dur = timedelta(seconds=self.session_duration)
+        time = g.created_at + dur
+        if time < datetime.now():
+            return None
+        return g.user_id
 
     def destroy_session(self, request=None):
         ''' destroy session '''
-        dl = super().destroy_session(request)
-        if dl:
-            return True
+        if request:
+            check = self.session_cookie(request)
+            if check:
+                to_del = UserSession.search({'session_id': check})
+                if to_del:
+                    dels = to_del[0]
+                    dels.remove()
+                    UserSession.save_to_file()
+                    return True
         return False
--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -7,5 +7,6 @@ class UserSession(Base):
     ''' users sessions '''
     def __init__(self, *args: list, **kwargs: dict):
         ''' define '''
+        super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
