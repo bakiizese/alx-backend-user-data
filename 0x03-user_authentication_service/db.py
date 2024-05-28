2b3e9492b634fa039c9ad11672346bb0aab954df
root <bereketzeselassie@gmail.com>
2024-05-28 12:43:23 +0000

    t

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,6 +33,12 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """ return user obj """
+        if not email or not hashed_password:
+            return None
+        elif (
+                not isinstance(email, str) or
+                not isinstance(hashed_password)):
+            return None
         u1 = User(email=email, hashed_password=hashed_password)
         self._session.add(u1)
         self._session.commit()
