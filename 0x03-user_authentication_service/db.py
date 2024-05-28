6bb659ecb26f332ec9868126e4464c9caaf952af
bakiizese <bereketezeselassie@gmial.com>
2024-05-28 23:24:48 +0300

    tr

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,16 +31,27 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
+    # def add_user(self, email: str, hashed_password: str) -> User:
+    #     """ return user obj """
+    #     try:
+    #         u1 = User(email=email, hashed_password=hashed_password)
+    #         self._session.add(u1)
+    #         self._session.commit()
+    #     except Exception:
+    #         self._session.rollback()
+    #         u1 = None
+    #     return u1
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ return user obj """
+        """Adds a new user to the database.
+        """
         try:
-            u1 = User(email=email, hashed_password=hashed_password)
-            self._session.add(u1)
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
             self._session.commit()
         except Exception:
             self._session.rollback()
-            u1 = None
-        return u1
+            new_user = None
+        return new_user
 
     def find_user_by(self, **kwargs) -> User:
         """ return whats found """
