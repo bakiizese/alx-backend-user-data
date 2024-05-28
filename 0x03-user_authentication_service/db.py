90576152716f30b8dc533fc1770a6862afa0e6f9
root <bereketzeselassie@gmail.com>
2024-05-28 10:26:13 +0000

    fixed

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from typing import TypeVar
+from typing import Optional
 from user import Base, User
 
 
