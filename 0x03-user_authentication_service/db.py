eaad79efcf289eb195f468e5e7936895cb3f279e
bakiizese <bereketezeselassie@gmial.com>
2024-05-28 22:56:12 +0300

    w

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -55,20 +55,20 @@ class DB:
             raise NoResultFound
         return qr
 
-    def update_user(self, ids: int, **kwargs) -> None:
-        ''' update a value '''
-        u1 = self.find_user_by(id=ids)
-        if not u1:
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates a user based on a given id.
+        """
+        user = self.find_user_by(id=user_id)
+        if user is None:
             return
-        cols = {}
-        for k, v in kwargs.items():
-            if hasattr(User, k):
-                cols[getattr(User, k)] = v
+        update_source = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                update_source[getattr(User, key)] = value
             else:
                 raise ValueError()
-        
-        self._session.query(User).filter(User.id == ids).update(
-                cols,
-                synchronize_session=False,
-            )
+        self._session.query(User).filter(User.id == user_id).update(
+            update_source,
+            synchronize_session=False,
+        )
         self._session.commit()
