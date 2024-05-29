9b773b346896e9add9a538bae2a50ee326c35f15
bakiizese <bereketezeselassie@gmial.com>
2024-05-29 06:37:46 +0300

    ps

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 ''' flask app '''
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from auth import Auth
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=["GET"], strict_slashes=False)
@@ -10,6 +11,13 @@ def index():
     ''' index route '''
     return jsonify({"message": "Bienvenue"})
 
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register():
+    '''register user'''
+    em = request.form.get('email')
+    ps = request.form.get('password')
+    print(f'{em},  {ps}')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
