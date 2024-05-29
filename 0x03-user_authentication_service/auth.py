4dcbf67657e212945e50de26233a6bcbfbdc54d8
bakiizese <bereketezeselassie@gmial.com>
2024-05-29 16:38:21 +0300

    password reseted

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,6 +64,16 @@ class Auth:
         self._db.update_user(usr.id, reset_token=reset_token)
         return usr.reset_token
 
+    def update_password(self, reset_token: str, password: str) -> None:
+        ''' update password '''
+        usr = self._db.find_user_by(reset_token=reset_token)
+        if not usr:
+            raise ValueError()
+        hashed_password = _hash_password(password)
+        self._db.update_user(usr.id, hashed_password=hashed_password)
+        self._db.update_user(usr.id, reset_token=None)
+        
+
 
 def _hash_password(password: str) -> bytes:
     ''' hash password by bcrypt '''
