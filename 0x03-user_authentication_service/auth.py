f8383c8ba08a1f9dd710ee74e48543e72d1c4a87
bakiizese <bereketezeselassie@gmial.com>
2024-05-29 16:13:59 +0300

    reset_token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,6 +53,16 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         ''' destroy sesion id by user id '''
         self._db.update_user(user_id, session_id=None)
+    
+    def get_reset_password_token(self, email: str) -> str:
+        ''' reset token '''
+        try:
+            usr = self._db.find_user_by(email=email)
+        except Exception:
+            raise ValueError()
+        reset_token = _generate_uuid()
+        self._db.update_user(usr.id, reset_token=reset_token)
+        return usr.reset_token
 
 
 def _hash_password(password: str) -> bytes:
