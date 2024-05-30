927c230a568bf0a0e8d3bbd34823826d9c6cf51e
bakiizese <bereketezeselassie@gmial.com>
2024-05-30 15:51:22 +0300

    check 4 err

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -97,14 +97,12 @@ def reset_pwd() -> str:
         abort(403)
     if not new_password:
         abort(403)
+
     try:
-        token = AUTH.get_reset_password_token(email)
-        if token is reset_token:
-             AUTH.update_password(reset_token, new_password)
-        else:
-            abort(403)
+        AUTH.update_password(reset_token, new_password)
     except ValueError:
         abort(403)
+
     ms: Dict = {"email": email, "message": "Password updated"}
     return jsonify(ms), 200
 
