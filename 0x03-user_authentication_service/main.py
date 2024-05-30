adfd0254b17821eb092103677010d022f6912794
bakiizese <bereketezeselassie@gmial.com>
2024-05-30 17:39:35 +0300

    main

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 ''' main module for every task '''
-from flask import request
-from auth import Auth
+import requests
+
+
+url = "http://192.168.1.4:5000/"
 
 def register_user(email: str, password: str) -> None:
-    pass
+    ''' check register user '''
+    url_1 = url + 'users'
+    data = {
+        'email': email,
+        'password': password
+    }
+    r = {"email": email, "message": "user created"}
+    r2 = {"message": "email already registered"}
+    response = requests.post(url_1, data=data)
+    assert response.json() == r
+    assert response.status_code == 200
+    response = requests.post(url_1, data=data)
+    assert response.status_code == 400
+    assert response.json() == r2
 
 def log_in_wrong_password(email: str, password: str) -> None:
     pass
@@ -36,11 +51,11 @@ NEW_PASSWD = "t4rt1fl3tt3"
 if __name__ == "__main__":
 
     register_user(EMAIL, PASSWD)
-    log_in_wrong_password(EMAIL, NEW_PASSWD)
-    profile_unlogged()
-    session_id = log_in(EMAIL, PASSWD)
-    profile_logged(session_id)
-    log_out(session_id)
-    reset_token = reset_password_token(EMAIL)
-    update_password(EMAIL, reset_token, NEW_PASSWD)
-    log_in(EMAIL, NEW_PASSWD)
+    #log_in_wrong_password(EMAIL, NEW_PASSWD)
+    #profile_unlogged()
+    #session_id = log_in(EMAIL, PASSWD)
+    #profile_logged(session_id)
+    #log_out(session_id)
+    #reset_token = reset_password_token(EMAIL)
+    #update_password(EMAIL, reset_token, NEW_PASSWD)
+    #log_in(EMAIL, NEW_PASSWD)
